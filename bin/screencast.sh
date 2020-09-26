e871beb33b260bf1f596b44a9244e1f750b5ac43
Henrik Aalto <hltk@hltk.fi>
2020-09-26 20:59:52 +0300

    add screencast.sh

---
+++ bin/screencast.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# FROM: https://www.reddit.com/r/i3wm/comments/7ahbvv/what_do_you_use_to_make_gif_screen_capture/
+
+TMPFILE="$(mktemp -t screencast-XXXXXXX).mkv"
+OUTPUT="$HOME/pics/$(date +%F-%H-%M-%S)"
+
+read -r X Y W H G ID < <(slop -f "%x %y %w %h %g %i")
+ffmpeg -f x11grab -s "$W"x"$H" -i :0.0+$X,$Y "$TMPFILE"
+
+notify-send 'generating palette'
+ffmpeg -y -i "$TMPFILE"  -vf fps=10,palettegen /tmp/palette.png
+notify-send 'generating gif'
+ffmpeg -i "$TMPFILE" -i /tmp/palette.png -filter_complex "paletteuse" $OUTPUT.gif
+mv $TMPFILE $OUTPUT.mkv
+
+notify-send "size $(du -h $OUTPUT.gif | awk '{print $1}')"
+
+eog $OUTPUT.gif
+
+trap "rm -f '$TMPFILE'" 0
