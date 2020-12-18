4649259d6bde054a2eb4ab2417f26a18399bb8e9
Henrik Aalto <hltk@hltk.fi>
2020-12-18 21:05:45 +0200

    fix fish's config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,13 +1,13 @@
 function rustc
-    rustc --color=never
+    command rustc --color=never $argv
 end
 
 function clang
-    clang -fno-color-diagnostics
+    command clang -fno-color-diagnostics $argv
 end
 
 function clang++
-    clang -fno-color-diagnostics
+    command clang -fno-color-diagnostics $argv
 end
 
 function l
@@ -19,7 +19,7 @@ function gdb
 end
 
 function dcm
-    g++ -Wall -Wextra -pedantic -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -fstack-protector -std=c++17 -g3 -fmax-errors=1 -O2
+    command g++ -Wall -Wextra -pedantic -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -fstack-protector -std=c++17 -g3 -fmax-errors=1 -O2 $argv
 end
 
 function mkcd
