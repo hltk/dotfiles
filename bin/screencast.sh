1851fc1135981d5219c7118cde72ecb82a6cb12a
Henrik Aalto <hltk@hltk.fi>
2020-12-26 17:18:01 +0200

    bin/screencast.sh: increase fps

--- bin/screencast.sh
+++ bin/screencast.sh
@@ -9,7 +9,7 @@ read -r X Y W H G ID < <(slop -f "%x %y %w %h %g %i")
 ffmpeg -f x11grab -s "$W"x"$H" -i :0.0+$X,$Y "$TMPFILE"
 
 notify-send 'generating palette'
-ffmpeg -y -i "$TMPFILE"  -vf fps=10,palettegen /tmp/palette.png
+ffmpeg -y -i "$TMPFILE"  -vf fps=30,palettegen /tmp/palette.png
 notify-send 'generating gif'
 ffmpeg -i "$TMPFILE" -i /tmp/palette.png -filter_complex "paletteuse" $OUTPUT.gif
 mv $TMPFILE $OUTPUT.mkv
