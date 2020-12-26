b7c538a56e5697a39fe6d2bfb7f5a7e9d0b06275
Henrik Aalto <hltk@hltk.fi>
2020-12-26 17:16:27 +0200

    switch to sway

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -2,4 +2,5 @@
 
 # from https://git.sr.ht/%7Eliebach/sde
 
-scrot '%Y-%m-%d_%H:%M:%S_$wx$h.png' -s -e 'mv $f ~/pics'
+# scrot '%Y-%m-%d_%H:%M:%S_$wx$h.png' -s -e 'mv $f ~/pics'
+grim -g "$(slurp)" $(xdg-user-dir PICTURES)/$(date +'%s_grim.png')
